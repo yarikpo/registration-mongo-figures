73d256a4825a63601621a9c136c502e128e11d63
yarikpo <yaroslavpopovich04@gmail.com>
2023-02-28 00:59:21 +0200

    first task -- done | added uploading zip file

---
+++ src/main/java/ua/clamor1s/registrationmongofigures/repository/RegisterRepository.java
@@ -0,0 +1,59 @@
+package ua.clamor1s.registrationmongofigures.repository;
+
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+import lombok.RequiredArgsConstructor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+@RequiredArgsConstructor
+public class RegisterRepository {
+
+    private final MongoTemplate mongoTemplate;
+
+    public void clearDatabase() {
+        mongoTemplate.remove(new Query(), "figures");
+    }
+
+    public void fillDatabaseFromInputStream(InputStream is) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+        try {
+            String json;
+
+            while ((json = bf.readLine()) != null) {
+                builder.append(json);
+            }
+        }
+        finally {
+            bf.close();
+        }
+
+        List<Document> documentList = convertJsonStringToDocumentList(builder.toString());
+        mongoTemplate.insert(documentList, "figures");
+    }
+
+    private List<Document> convertJsonStringToDocumentList(String json) {
+        List<Document> documentList = new ArrayList<>();
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); ++i) {
+            JSONObject jsonDocument = jsonArray.getJSONObject(i);
+            documentList.add(Document.parse(jsonDocument.toString()));
+        }
+
+        return documentList;
+    }
+
+}
--- src/main/java/ua/clamor1s/registrationmongofigures/service/RegisterServiceImpl.java
+++ src/main/java/ua/clamor1s/registrationmongofigures/service/RegisterServiceImpl.java
@@ -1,12 +1,12 @@
 package ua.clamor1s.registrationmongofigures.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import ua.clamor1s.registrationmongofigures.repository.RegisterRepository;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -14,12 +14,16 @@ import java.util.zip.ZipInputStream;
 @Service
 public class RegisterServiceImpl implements RegisterService {
 
+    @Autowired
+    private RegisterRepository repository;
 
     @Override
     public ResponseEntity<?> uploadZipToDatabase(MultipartFile file) {
+        repository.clearDatabase();
         String fileName = file.getOriginalFilename();
         try {
-            getJsonFileFromZipStream(file.getInputStream());
+            InputStream is = getJsonFileDataStreamFromZipStream(file.getInputStream());
+            repository.fillDatabaseFromInputStream(is);
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -30,7 +34,9 @@ public class RegisterServiceImpl implements RegisterService {
         return ResponseEntity.ok("oks... file: %s".formatted(fileName));
     }
 
-    private void getJsonFileFromZipStream(InputStream is) throws IOException {
+
+
+    private InputStream getJsonFileDataStreamFromZipStream(InputStream is) throws IOException {
         ZipInputStream zis = new ZipInputStream(is);
 
         for (ZipEntry zipEntry; (zipEntry = zis.getNextEntry()) != null;) {
@@ -38,12 +44,9 @@ public class RegisterServiceImpl implements RegisterService {
                 continue;
             }
             // here we will see data exactly from pep.json file (if there are more
-            // than one file in zip archive)
-            Scanner sc = new Scanner(zis);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
-            }
-            return;
+            // than one files in zip archive)
+
+            return zis;
         }
 
         throw new IOException("File pep.json not found.");
